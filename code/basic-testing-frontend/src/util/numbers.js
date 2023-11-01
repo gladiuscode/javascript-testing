eeb34da81e60a17b99869b4cab1cac910a4ec472
gladiuscode <gladiusservices@protonmail.com>
2023-11-01 11:14:22 +0100

    feat: continue app.js refactor with cleanNumbers function

--- code/basic-testing-frontend/src/util/numbers.js
+++ code/basic-testing-frontend/src/util/numbers.js
@@ -1,4 +1,16 @@
+import {validateNumber, validateStringNotEmpty} from "./validation.js";
+
 export function transformToNumber(value) {
   return +value;
 }
 
+export function cleanNumbers(input) {
+  const numbers = [];
+  for (const numberInput of input) {
+    validateStringNotEmpty(numberInput);
+    const number = transformToNumber(numberInput);
+    validateNumber(number);
+    numbers.push(number);
+  }
+  return numbers;
+}

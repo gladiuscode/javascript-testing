8320489ac6b5c91ce3ef8868e4c554b56625ad15
gladiuscode <gladiusservices@protonmail.com>
2023-11-01 13:40:28 +0100

    feat: move duplicated code outside and demonstrate how it breaks the execution

--- code/03-async-code-starting-project/hooks/hooks.test.js
+++ code/03-async-code-starting-project/hooks/hooks.test.js
@@ -2,45 +2,43 @@ import { it, expect } from 'vitest';
 
 import { User } from './hooks.js';
 
+/*
+  To cleanup duplicate code we could move our testEmail const
+  and user outside of the it functions, as follow.
+  Although by moving them out, we are breaking our tests
+  because we are not initializing a new user each time
+  we run each it function, so the user object is dirty
+  with all the operations apply to it by the previous
+  it functions.
+  This is why it doesn't work.
+ */
+
+const testEmail = 'test@test.com';
+const user = new User(testEmail);
+
 it('should update the email', () => {
-  const testEmail = 'test@test.com';
   const newTestEmail = 'test2@test.com';
 
-  const user = new User(testEmail);
   user.updateEmail(newTestEmail);
 
   expect(user.email).toBe(newTestEmail);
 });
 
 it('should have an email property', () => {
-  const testEmail = 'test@test.com';
-
-  const user = new User(testEmail);
-
   expect(user).toHaveProperty('email');
 });
 
 it('should store the provided email value', () => {
-  const testEmail = 'test@test.com';
-
-  const user = new User(testEmail);
-
   expect(user.email).toBe(testEmail);
 });
 
 it('should clear the email', () => {
-  const testEmail = 'test@test.com';
-
-  const user = new User(testEmail);
   user.clearEmail();
 
   expect(user.email).toBe('');
 });
 
 it('should still have an email property after clearing the email', () => {
-  const testEmail = 'test@test.com';
-
-  const user = new User(testEmail);
   user.clearEmail();
 
   expect(user).toHaveProperty('email');

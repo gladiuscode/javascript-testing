ab28dbfc57d9f48985a2e2cadc9c87f51c751038
gladiuscode <gladiusservices@protonmail.com>
2023-11-01 12:53:15 +0100

    feat: implement an integration test

--- code/basic-testing-frontend/src/util/numbers.test.js
+++ code/basic-testing-frontend/src/util/numbers.test.js
@@ -1,20 +1,56 @@
-import { it, expect } from 'vitest';
-import { transformToNumber } from "./numbers";
+import {it, expect, describe} from 'vitest';
+import {cleanNumbers, transformToNumber} from "./numbers";
 
-it('should yield a valid string number as a valid number', () => {
-    const stringNumber = '1';
-    const expectedResult = +stringNumber;
+describe('transformToNumber()', () => {
+    it('should yield a valid string number as a valid number', () => {
+        const stringNumber = '1';
+        const expectedResult = +stringNumber;
 
-    const result = transformToNumber(stringNumber);
+        const result = transformToNumber(stringNumber);
 
-    expect(result).toBe(expectedResult);
-    // expect(result).toBeTypeOf('number');
+        expect(result).toBe(expectedResult);
+        // expect(result).toBeTypeOf('number');
+    });
+
+    it('should yield NaN if provided with an undefined argument', () => {
+        const input = undefined;
+
+        const result = transformToNumber(input);
+
+        expect(result).toBeNaN();
+    });
 });
 
-it('should yield NaN if provided with an undefined argument', () => {
-    const input = undefined;
+/*
+    This can be called Integration Test, since we are testing a function that leverages smallest
+    units to properly work. Integration Test are valid because even when units work
+    correctly, we may introduce an error into this macro function.
+    They allow us to test the combination of the smallest units.
+ */
+describe('cleanNumbers()', () => {
+    it('should retrun an array of number values if ana array of string number values is provided', () => {
+        const numberValues = ['1', '2'];
 
-    const result = transformToNumber(input);
+        const result = cleanNumbers(numberValues);
 
-    expect(result).toBeNaN();
+        expect(result[0]).toBeTypeOf('number');
+    })
+
+    it('should throw an error if an array with at least one empty string is provided', () => {
+        const numberValues = ['', ''];
+
+        const resultFn = () => cleanNumbers(numberValues);
+
+        expect(resultFn).toThrow();
+    })
 });
+
+/*
+    Integration Test vs Unit Test
+
+    We should try to be as granular as possible, therefore splitting up our code when
+    it DOES make sense, when we can achieve a better readability and testability of the
+    standalone sub-features that make up our feature in the end.
+    We can nail down errors by being granular, although we should test the main
+    macro functions that leverage many units.
+ */
